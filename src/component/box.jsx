525ae2c4cfc050b22fb037dc7a37f7cac5b0e62a
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-07-07 22:51:53 +0300

    refactor: :art: refactor code

--- src/component/box.jsx
+++ src/component/box.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Cart from "./Cart";
 import Modal from "./common/modal";
 import './Box.css';
@@ -6,39 +6,37 @@ import './Box.css';
 class Box extends Component {
     constructor(props) {
         super(props);
-        this.state = {show: false}
+        this.state = { show: false }
     }
 
 
     render() {
-        let {show} = this.state;
-        let {task: tasks} = this.props;
+        let { show } = this.state;
+        let { task: tasks, label } = this.props;
         return (
-            <React.Fragment>
                 <div className='Box col-4 container'>
                     <Modal title='New task'
-                           status={this.props.label}
-                           show={show}
-                           onShow={this.handleShow}
+                        status={label}
+                        show={show}
+                        onShow={this.handleShow}
                     />
                     <div className="box-header">
-                        <strong className="label">{this.props.label}</strong>
+                        <strong className="label">{label}</strong>
                         <div className="btn btn-dark bi-plus add-btn text-white-50"
-                             onClick={this.handleShow}>
-                            Add item
-                        </div>
+                            onClick={this.handleShow}/>
                     </div>
                     <div className="box-body">
                         {tasks.map(task => (
                             <Cart
+                                key={task.title + Math.random(task.description)}
                                 title={task.title}
                                 description={task.description}
+                                status={label}
                             />
                         ))}
                     </div>
 
                 </div>
-            </React.Fragment>
         )
             ;
     }
@@ -46,8 +44,8 @@ class Box extends Component {
     handleShow = () => {
         // console.log('before', this.state.show)
         if (this.state.show === false) {
-            this.setState({show: true});
-        } else this.setState({show: false})
+            this.setState({ show: true });
+        } else this.setState({ show: false })
         // console.log('after', this.state.show)
     }
 }
